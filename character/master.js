b9be4db5ea25d5ae38033c57cc5273b182b4f656
Muhammad Farhan <muh.farhan0852@gmail.com>
2020-03-16 16:37:48 +0700

    Test Script sync mode

---
+++ character/master.js
@@ -0,0 +1,25 @@
+
+if (operator.gclass == "caster") {
+    document.getElementById("classCaster").style.display = "block";
+}
+if (operator.gclass == "defender") {
+    document.getElementById("classDefender").style.display = "block";
+}
+if (operator.gclass == "guard") {
+    document.getElementById("classGuard").style.display = "block";
+}
+if (operator.gclass == "medic") {
+    document.getElementById("classMedic").style.display = "block";
+}
+if (operator.gclass == "sniper") {
+    document.getElementById("classSniper").style.display = "block";
+}
+if (operator.gclass == "specialist") {
+    document.getElementById("classSpecialist").style.display = "block";
+}
+if (operator.gclass == "supporter") {
+    document.getElementById("classSupporter").style.display = "block";
+}
+if (operator.gclass == "vanguard") {
+    document.getElementById("classVanguard").style.display = "block";
+}
